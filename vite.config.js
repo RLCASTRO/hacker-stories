fd649de3071ba2b7d8072840fa447b9db196420c
Rodrigo Castro <castro.rl@gmail.com>
2023-04-08 21:28:15 -0300

    publishing to ghpages config set up successfully

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://RLCASTRO.github.io/hacker-stories",
   plugins: [react()],
 })
